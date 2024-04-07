85915add7130c6579ac986a3eea041edacf46d6c
ChottoMatte <jin92370@gmail.com>
2024-04-07 20:01:27 +0900

    作成日時と更新日時を追加

--- src/main/resources/db/migration/V3__books_authors.sql
+++ src/main/resources/db/migration/V3__books_authors.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS `books_authors` (
     `book_id` int(10) NOT NULL ,
     `author_id` int(10) NOT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`book_id`, `author_id`),
     FOREIGN KEY (`book_id`) REFERENCES books(`book_id`),
     FOREIGN KEY (`author_id`) REFERENCES authors(`author_id`)
